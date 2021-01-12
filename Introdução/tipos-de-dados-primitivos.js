461d07c82ded11afe7333a4d0d884c46f39d89b0
figueiredosoftware <figueiredosoftware@gmail.com>
2021-01-12 00:04:09 -0300

    commit 20210112

--- Introdução/tipos-de-dados-primitivos.js
+++ Introdução/tipos-de-dados-primitivos.js
@@ -13,3 +13,18 @@ console.log(minhavar3);
 
 var minhavar4 = "minha \"string\" com aspas duplas escapando caracter";
 console.log(minhavar4);
+
+var minhavar5 = `minha template literal`;
+console.log(minhavar5);
+
+//concatenação
+var idade = 40;
+var msg = "eu possuo " + idade +  " anos";
+console.log(msg);
+
+var idade = 40;
+var msg = `eu possuo ${idade} anos`;
+console.log(msg);
+
+console.log ("Hello " + "World");
+
