6797d6a924e805e33a2a2ff7fb45b1faea62c660
figueiredosoftware <figueiredosoftware@gmail.com>
2021-02-11 21:20:13 -0300

    commit 11/02/2021

---
+++ Introdução/6-operadoresDeAtribuicao.js
@@ -0,0 +1,28 @@
+// =  : IGUAL
+
+
+var n1 = 20; //atribui 10 para n1
+console.log(`n1 = 20 : ${n1}`);
+console.log("tipo de n1 : " + typeof(n1));
+
+n1 = n1 + 15;
+console.log(`n1 = n1 + 15 : ${n1}`);
+//ou
+n1 += 15;
+console.log(`n1 += 15 : ${n1}`);
+
+n1 -= 5;
+console.log(`n1 -= 5 : ${n1}`);
+
+n1 *= 2;
+console.log(`n1 *= 2 : ${n1}`);
+
+n1 /= 3;
+console.log(`n1 /= 3 : ${n1}`);
+
+n1 %= 4;
+console.log(`n1 %= 4 : ${n1}`);
+
+n1 **= 3;
+console.log(`n1 **= 3 : ${n1}`);
+
