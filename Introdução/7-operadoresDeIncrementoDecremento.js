b204a97543a1e76866da4e8f02ad5c963bf771c2
figueiredosoftware <figueiredosoftware@gmail.com>
2021-02-13 21:18:08 -0300

    Commit 13/02/2021

---
+++ Introdução/7-operadoresDeIncrementoDecremento.js
@@ -0,0 +1,19 @@
+var i = 5;
+console.log("Valor e i depois de var i = 5:" + i);
+//PRE Incremento
+console.log("Valor e i depois de var ++i dentro do console log:" + ++i);
+console.log("Valor e i normal:" + i);
+//PRE Decremento
+console.log("Valor e i depois de var --i dentro do console log:" + --i);
+console.log("Valor e i normal:" + i);
+//POS Incremento
+console.log("Valor e i depois de var i++ dentro do console log:" + i++);
+console.log("Valor e i normal:" + i);
+//POS Decremento
+console.log("Valor e i depois de var i-- dentro do console log:" + i--);
+console.log("Valor e i normal:" + i);
+
+
+
+
+
