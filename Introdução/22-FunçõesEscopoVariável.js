169cab431b8be7c428d8832bb5ce320242f46060
figueiredosoftware <figueiredosoftware@gmail.com>
2021-06-01 11:01:17 -0300

    commit 01/06/2021

--- Introdução/22-FunçõesEscopoVariável.js
+++ Introdução/22-FunçõesEscopoVariável.js
@@ -1,3 +1,5 @@
+//VAR gera escopo de função e LET e CONST gera escopo de bloco
+
 var n = "global";
 
 function mostarNGlobal(){
@@ -9,6 +11,25 @@ function mostarNlocal(){
     var n="local";
     console.log("Valor de n : " + n);
 
+    // por causa de usar LET a variável n1 interna está no escopo de bloco dentro do if
+    let n1 = "n1 local";
+    console.log("Valor de n1 " + n1);
+
+    if(true){
+        let n1 = "dentro de n1 dentro de if com let";//usando let o n1 dentro do if é outra variável fora do escopo de n1 fora do if
+    }
+    console.log("valor de n1 : " + n1);
+
+
+    //se usar VAR a variável n2 interna está no MESME escopo de FORA DO bloco dentro do if
+    var n2 = "n2 local";
+    console.log("Valor de n2 " + n2);
+
+    if(true){
+        var n2 = "dentro de n2 dentro de if com VAR";//usando VAR n2 dentro do if é A MESMA variável fora do escopo de n2 fora do if
+    }
+    console.log("valor de n2 : " + n2);
+
 }
 
 mostarNGlobal();
@@ -16,6 +37,7 @@ mostarNGlobal();
 mostarNlocal();
 
 console.log("Valor de n : " + n);
+//console.log("Valor de n1 : " + n1);
 
 
 
