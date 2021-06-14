8fb8ed983545acdb105f15528696d5fabfb15332
figueiredosoftware <figueiredosoftware@gmail.com>
2021-06-14 20:29:12 -0300

    commit 14/06/2021

--- Introdução/23-arrays.js
+++ Introdução/23-arrays.js
@@ -10,3 +10,25 @@ console.log(arr2);
 console.log(arr2[1]);
 console.log(arr2[3][1]);
 console.log(arr2[3][3]);
+console.log(arr2.length);
+console.log(arr2[3].length);
+console.log(arr2[3][arr2[3].length-1]);
+
+const arr3 = ["Dani", 28, [3, 6, 9], true];
+console.log(arr3);
+console.log(arr3[2]);
+console.log(arr3[2][1]);
+
+arr3[4] = "vlr 1";
+console.log(arr3);
+arr3[arr3.length] = "vlr2";
+console.log(arr3);
+arr3.push("push");
+console.log(arr3);
+arr3.push("k1", "k2", "k3");
+console.log(arr3);
+
+console.log(arr3[6]);
+
+var n = 6;
+console.log(arr3[n]);
